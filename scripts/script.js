26f35552950bfac6c054ca1902724d4511666deb
Mr Rover <fercruz.ortega@gmail.com>
2014-09-19 19:18:45 -0500

    almost done

---
+++ scripts/script.js
@@ -0,0 +1,17 @@
+var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+
+window.addEvent('load', function() {
+});
+
+window.addEvent("load", function(){
+});
+
+window.addEvent("resize", function(){
+});
+
+window.addEvent('orientationchange',function() {
+});
+
+window.addEvent('scroll',function(){
+});
