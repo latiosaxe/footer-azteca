37d12ccd6f2466dda2421eab0205db5292fb8321
latiosaxe <latiosaxe@gmail.com>
2014-09-23 10:58:32 -0500

    Footer para entrega

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,12 +1,10 @@
 window.addEvent('domready', function() {
 
-    $$(".expand > a").addEvent('click', function(){
+    $$(".expand > a").addEvent('click', function(e){
+        e.preventDefault();
         var hijo = $(this).getParent('.expand').getChildren('.vertical_slide')[0];
-        console.log();
-
         if(typeof hijo === 'undefined'){
         }else{
-//            $$(".vertical_slide").removeClass('expanded');
             if(hijo.hasClass('expanded')){
                 hijo.removeClass('expanded');
             }else{
